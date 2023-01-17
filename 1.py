86580cf092f374ce2702876d8a337d53c62d624f
burmistrov1337 <burmistrov.ma@mail.ru>
2023-01-17 14:20:31 +0700

    1

--- 1.py
+++ 1.py
@@ -16,3 +16,22 @@ print(f'n = {n}')
 
 P=m/n
 print(f'P(4 трефы) = {round(P,4)}')
+
+P1=13/52*12/51*11/50*10/49
+print(f'P(4 трефы) = {round(P1,4)}')
+
+# б) Найти вероятность, что среди 4-х карт окажется хотя бы один туз.
+
+n=combinations(52, 4)
+print(f'n = {n}')
+
+m=sum([combinations(4,1)*combinations(48,3),combinations(4,2)*combinations(48,2),combinations(4,3)*combinations(48,1),1])
+print(f'm = {m}')
+
+P=m/n
+print(f'P(хотя бы 1 туз) = {round(P,4)}')
+
+P2=1-combinations(48,4)/combinations(52,4)
+print(f'P(хотя бы 1 туз) = {round(P2,4)}')
+
+print(f'Ответы: а) P(4 трефы) = {round(P1,4)},' /n 'б) P(хотя бы 1 туз) = {round(P,4)}')
