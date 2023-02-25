a06aafcee25463fd6b572c13aa06326fc20be485
burmistrov1337 <burmistrov.ma@mail.ru>
2023-02-25 23:26:41 +0700

    dz10

--- 10.py
+++ 10.py
@@ -6,16 +6,10 @@
 # Хоккеисты: 177, 179, 180, 188, 177, 172, 171, 184, 180. 
 # Штангисты: 172, 173, 169, 177, 166, 180, 178, 177, 172, 166, 170.
 
-import numpy as np
 import pandas as pd
+import numpy as np
 from scipy import stats
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pylab import rcParams
-sns.set()
-rcParams['figure.figsize'] = 10, 6
-%config InlineBackend.figure_format = 'svg'
-np.random.seed(42)
+from scipy.stats import f_oneway
 
 football=np.array([173, 175, 180, 178, 177, 185, 183, 182])
 hockey=np.array([177, 179, 180, 188, 177, 172, 171, 184, 180])
@@ -23,4 +17,4 @@ weightlifting=np.array([172, 173, 169, 177, 166, 180, 178, 177, 172, 166, 170])
 
 stats.f_oneway(football, hockey, weightlifting)
 
-F_onewayResult(statistic=5.500053450812596, pvalue=0.010482206918698694)
+f_onewayResult(statistic=5.500053450812596, pvalue=0.010482206918698694)
